c02c875cf327b334ae1dad844fb0a3a6f98880e0
Taha Al Khashmany <taha.al.khashmany@signpath.io>
2025-03-11 09:18:21 +0100

    fix vcs root

--- .teamcity/settings.kts
+++ .teamcity/settings.kts
@@ -1,32 +1,9 @@
 import jetbrains.buildServer.configs.kotlin.*
 import jetbrains.buildServer.configs.kotlin.buildSteps.powerShell
 
-/*
-The settings script is an entry point for defining a TeamCity
-project hierarchy. The script should contain a single call to the
-project() function with a Project instance or an init function as
-an argument.
-
-VcsRoots, BuildTypes, Templates, and subprojects can be
-registered inside the project using the vcsRoot(), buildType(),
-template(), and subProject() methods respectively.
-
-To debug settings scripts in command-line, run the
-
-    mvnDebug org.jetbrains.teamcity:teamcity-configs-maven-plugin:generate
-
-command and attach your debugger to the port 8000.
-
-To debug in IntelliJ Idea, open the 'Maven Projects' tool window (View
--> Tool Windows -> Maven Projects), find the generate task node
-(Plugins -> teamcity-configs -> teamcity-configs:generate), the
-'Debug' option is available in the context menu for the task.
-*/
-
 version = "2024.03"
 
 project {
-
     buildType(TahasTestsProject_VersionedSettings_Test)
 }
 
@@ -37,13 +14,11 @@ object TahasTestsProject_VersionedSettings_Test : BuildType({
     artifactRules = "signed.dll"
 
     params {
-        param("SignPathApiToken", "ABj3ppzgBQ0Lq1s8LSadGZYIpikXelEK+W3OBVGqoe9u")
-        param("param1", "value1")
-        param("param2", "value2")
+
     }
 
     vcs {
-        root(AbsoluteId("TahasTestsProject_HttpsGithubComCycloneDXCyclonedxCliGit"))
+        root(DslContext.settingsRoot)
     }
 
     steps {
@@ -58,6 +33,7 @@ object TahasTestsProject_VersionedSettings_Test : BuildType({
                 """.trimIndent()
             }
         }
+
         step {
             id = "SignPathRunner_2"
             type = "SignPathRunner"

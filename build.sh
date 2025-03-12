bf24273521e62c4a0269bbc7f3ed64791d1960c7
Taha Al Khashmany <taha.al.khashmany@signpath.io>
2025-03-12 11:22:32 +0100

    ensure clean package

--- build.sh
+++ build.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
 buildOutputDirectory="BuildOutput"
+package=BuildOutput.zip
 
 rm $buildOutputDirectory -R -f
+rm $package -f
 
 dotnet publish "HelloWorld" -o $buildOutputDirectory --self-contained
 
-zip -r BuildOutput.zip BuildOutput/*
+zip -r $package $buildOutputDirectory

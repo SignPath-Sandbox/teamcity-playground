1b763ca2f53914a95958ee7c9a585e1d3b399104
Taha Al Khashmany <taha.al.khashmany@signpath.io>
2025-03-13 17:42:38 +0100

    New SignPath organization

--- .teamcity/settings.kts
+++ .teamcity/settings.kts
@@ -34,8 +34,8 @@ object Test : BuildType({
             param("outputArtifactPath", artifact)
 
             param("artifactConfigurationSlug", "initial")
-            param("organizationId", "7cf15ea1-dc50-4d3d-b0c1-e88a5a7d951c")
-            param("apiToken", "credentialsJSON:8d2abf33-2c18-49a8-94e2-10fd378478e7")
+            param("organizationId", "d0bc0910-ab91-4b74-bcdd-52d983196a4d")
+            param("apiToken", "credentialsJSON:24fb3992-424d-4a9d-ab99-068eccb8e3fe")
 
             param("waitForCompletion", "true")
             param("connectorUrl", "https://teamcity-connector-playground.customersimulation.int.signpath.io")

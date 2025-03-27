bed6d7ab5da02bd12fa8fbe692a7674b2d02d721
Taha Al Khashmany <taha.al.khashmany@signpath.io>
2025-03-27 10:38:14 +0100

    Test kotlin dsl for SignPath step

--- .teamcity/settings.kts
+++ .teamcity/settings.kts
@@ -1,5 +1,6 @@
 import jetbrains.buildServer.configs.kotlin.*
 import jetbrains.buildServer.configs.kotlin.buildSteps.script
+import jetbrains.buildServer.configs.kotlin.buildSteps.signpath
 
 version = "2024.03"
 
@@ -25,22 +26,39 @@ object Test : BuildType({
             formatStderrAsError = true
         }
 
-        step {
-            val artifact = "BuildOutput.zip"
+        // https://teamcity-playground.customersimulation.int.signpath.io/app/dsl-documentation/buildSteps/signpath-step/index.html
+         signpath {
+             val artifact = "BuildOutput.zip"
+             name = "Sign Package"
 
-            id = "SignPathRunner_2"
-            type = "SignPathRunner"
-            param("inputArtifactPath", artifact)
-            param("outputArtifactPath", artifact)
+             inputArtifactPath = artifact
+             outputArtifactPath = artifact
+             artifactConfigurationSlug = "initial"
+             organizationId = "d0bc0910-ab91-4b74-bcdd-52d983196a4d"
+             apiToken = "credentialsJSON:795ac364-d1c2-46f6-9bc4-d9967e28ef8b"
 
-            //param("artifactConfigurationSlug", "initial")
-            param("organizationId", "d0bc0910-ab91-4b74-bcdd-52d983196a4d")
-            param("apiToken", "credentialsJSON:795ac364-d1c2-46f6-9bc4-d9967e28ef8b")
+             waitForCompletion = true
+             connectorUrl = "https://teamcity-connector-playground.customersimulation.int.signpath.io"
+             signingPolicySlug = "test-signing"
+             projectSlug = "TeamCityConnector"
+         }
 
-            param("waitForCompletion", "true")
-            param("connectorUrl", "https://teamcity-connector-playground.customersimulation.int.signpath.io")
-            param("signingPolicySlug", "test-signing")
-            param("projectSlug", "TeamCityConnector")
-        }
+//        step {
+//            val artifact = "BuildOutput.zip"
+//
+//            id = "SignPathRunner_2"
+//            type = "SignPathRunner"
+//            param("inputArtifactPath", artifact)
+//            param("outputArtifactPath", artifact)
+//
+//            //param("artifactConfigurationSlug", "initial")
+//            param("organizationId", "d0bc0910-ab91-4b74-bcdd-52d983196a4d")
+//            param("apiToken", "credentialsJSON:795ac364-d1c2-46f6-9bc4-d9967e28ef8b")
+//
+//            param("waitForCompletion", "true")
+//            param("connectorUrl", "https://teamcity-connector-playground.customersimulation.int.signpath.io")
+//            param("signingPolicySlug", "test-signing")
+//            param("projectSlug", "TeamCityConnector")
+//        }
     }
 })
